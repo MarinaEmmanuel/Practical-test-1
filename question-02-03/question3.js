b2875665ecd0b5a326223b2889f91ab9f7e08937
MarinaEmmanuel <109877863+MarinaEmmanuel@users.noreply.github.com>
2022-08-27 00:33:41 +1200

    first commit

---
+++ question-02-03/question3.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", function () {
+
+    // TODO (B) Create a function called addCard(), which takes a single concert as an argument and adds a card to the
+    // #container div to display that concert to the user. For reference, a card's HTML looks like:
+    /*
+
+        <div class="card">
+            <div class="card-header"><a href="CONCERT_URL"><img class="card-image" src="./assets/CONCERT_IMAGE_NAME"></a></div>
+            <div class="card-body">
+                <h1 class="card-title">CONCERT_NAME</h1>
+                <p class="card-text">CONCERT_DESCRIPTION</p>
+            </div>
+        </div>
+
+    */
+
+    // TODO (C) Loop through all concerts in the concerts array (see question3-concerts.js). For each one,
+    // call the addCard() function, supplying that concert as a parameter.
+
+});
